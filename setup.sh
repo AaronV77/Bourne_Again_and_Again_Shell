4b2a79ad1c1825068b3225117b285ea3fd0478e2
AaronV77 <aaronvaloroso77@gmail.com>
2018-03-05 21:03:39 -0600

    Setup script udpates

--- setup.sh
+++ setup.sh
@@ -1,54 +1,59 @@
 #/bin/sh
 
-/*-------------------------------------------------------------------
-Author: Aaron Anthony Valoroso
-Date: March 4th, 2018
-License: BSD-3-Clause
-Email: valoroso99@gmail.com
---------------------------------------------------------------------*/
+#/*-------------------------------------------------------------------
+#Author: Aaron Anthony Valoroso
+#Date: March 4th, 2018
+#License: BSD-3-Clause
+#Email: valoroso99@gmail.com
+#--------------------------------------------------------------------*/
 
 dir=$(pwd -P)
 build_dir="${dir}/build"
 var=$(id)
 
-if [ "$var" = "uid=0(root) gid=0(root) groups=0(root)" ];
+echo "Welcome to Thursday!"
+read -p "Which system are we working on (Offline / Online / Usage): " system_options
+echo "Please choose one of the following options: "
+echo -e "\tInstall  - 1"
+echo -e "\tUpdate   - 2"
+echo -e "\tUnintall - 3"
+read -p "What operation are we doing: " user_option
+
+if [ "$system_options" = "Usage" ] || [ "$system_options" = "usage" ];
 then
-	echo "Welcome to Thursday!"
-	read -p "Which system are we working on (Offline / Online / Usage): " system_options
-	echo "Please choose one of the following options: "
-	echo -e "\tInstall  - 1"
-	echo -e "\tUpdate   - 2"
-	echo -e "\tUnintall - 3"
-	read -p "What operation are we doing: " user_option
-	if [ "$system_options" = "Usage" ] || [ "$system_options" = "usage" ];
+	echo "Offline - This is for the system that runs on top of the Bash Shell."
+	echo "Online - This is for the system that runs on its own dedicated Shell."
+else
+	if [ "$system_options" = "Offline" ] || [ "$system_options" = "offline" ]; 
 	then
-		echo "Offline - This is for the system that runs on top of the Bash Shell."
-		echo "Online - This is for the system that runs on its own dedicated Shell."
-	else
-		if [ "$system_options" = "Offline" ] || [ "$system_options" = "offline" ]; 
+		if [ $user_option = 1 ] || [ $user_option = 2 ]; 
 		then
-			if [ $user_option = 1 ] || [ $user_option = 2 ]; 
+			if [ ! -d "build" ]; 
 			then
-				if [ ! -d "build" ]; 
-				then
-					mkdir build
-				fi
-				cd Offline
-				cd src
-				make
-				mv Thurs $build_dir
-				mv Thursday.o $build_dir
-				mv Utilities.o $build_dir
-				cd $dir
-			elif [ $user_option = 3 ];
+				mkdir build
+			fi
+			cd Offline
+			cd src
+			make
+			mv Thurs $build_dir
+			mv Thursday.o $build_dir
+			mv Utilities.o $build_dir
+			cd $dir
+			if [ "$var" = "uid=0(root) gid=0(root) groups=0(root)" ];
 			then
-				if [ -d "build" ]; then
-					rm -rf build
-				fi
-			else
-				echo "The option you chose is not a valid option."
-			fi 
-		elif [ "$system_options" = "Online" ] || [ "$system_options" = "online" ];
+				echo "Since you are root, you will need to root in order to remove the build directory."
+			fi
+		elif [ $user_option = 3 ];
+		then
+			if [ -d "build" ]; then
+				rm -rf build
+			fi
+		else
+			echo "The option you chose is not a valid option."
+		fi 
+	elif [ "$system_options" = "Online" ] || [ "$system_options" = "online" ];
+	then
+		if [ "$var" = "uid=0(root) gid=0(root) groups=0(root)" ];
 		then
 			if [ $user_option = 1 ] || [ $user_option = 2 ]; 
 			then
@@ -73,11 +78,11 @@ then
 				cd $dir
 			else
 				echo "The option you chose is not a valid option."
-			fi 
+			fi
 		else
-			echo "Sorry, did not understand your input."
+			echo "Sorry, this script needs root privileges."
 		fi
+	else
+		echo "Sorry, did not understand your input."
 	fi
-else 
-	echo "Sorry, this script needs root privileges."
 fi
